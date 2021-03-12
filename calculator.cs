82b8e933f22110d151795e2304ad7758fc9c48a6
AlfinMH <alfinmhasan07@students.amikom.ac.id>
2021-03-12 13:19:28 +0700

    enambah perkalian dan pembagian

--- calculator.cs
+++ calculator.cs
@@ -7,6 +7,8 @@ class calculator
 
         Console.Writeline("Hasil Penambahan {0} + {1} = {2}"a, b, Penambahan(a, b));
         Console.Writeline("Hasil Pengurangan {0} - {1} = {2}"a, b, Pengurangan(a, b));
+        Console.Writeline("Hasil Perkalian {0} * {1} = {2}"a, b, Perkalian(a, b));
+        Console.Writeline("Hasil Pembagian {0} / {1} = {2}"a, b, Pembagian(a, b));
 
         Console.Writeline("\nTekan sembarang key untuk keluar")
         Console.Readkey();
@@ -18,5 +20,13 @@ class calculator
     static int Pengurangan(int a, int b)
     {
         return a - b;
+    }
+    static int Perkalian(int a, int b)
+    {
+        return a * b;
+    }
+    static int Pembagian(int a, int b)
+    {
+        return a / b;
     }
 }
